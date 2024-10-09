488d0e287dcd2da6eec84e9db7de68ba6f30f221
quiatus <quiatus@gmail.com>
2024-10-09 21:59:05 +0200

    add fps limiter

--- 02-asteroids/main.py
+++ 02-asteroids/main.py
@@ -4,6 +4,8 @@ from constants import *
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+fps = pygame.time.Clock()
+dt = 0
 
 def main():
   while True:
@@ -12,6 +14,9 @@ def main():
         return
     pygame.Surface.fill(screen, (0,0,0))
     pygame.display.flip()
+
+    dt = fps.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
