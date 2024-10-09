31a7c3f209198bdc1a20044139a79ed8687e6f4d
quiatus <quiatus@gmail.com>
2024-10-09 22:40:28 +0200

    add groups

--- 02-asteroids/main.py
+++ 02-asteroids/main.py
@@ -8,8 +8,13 @@ def main():
   pygame.init()
 
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   fps = pygame.time.Clock()
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+  Player.containers = (updatable, drawable)
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
   dt = 0
 
   while True:
@@ -17,9 +22,14 @@ def main():
       if event.type == pygame.QUIT:
         return
     
-    player.update(dt)
+    for obj in updatable:
+      obj.update(dt)
+
     screen.fill("black")
-    player.draw(screen)
+
+    for obj in drawable:
+      obj.draw(screen)
+
     pygame.display.flip()
 
     dt = fps.tick(60) / 1000
