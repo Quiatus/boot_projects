249eb2a019435ffaa6a0aa7df645d3ad86aa1532
quiatus <quiatus@gmail.com>
2024-10-09 22:16:08 +0200

    draw player

--- 02-asteroids/main.py
+++ 02-asteroids/main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import *
+from player import *
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 fps = pygame.time.Clock()
 dt = 0
 
@@ -12,7 +14,8 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
-    pygame.Surface.fill(screen, (0,0,0))
+    screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
 
     dt = fps.tick(60) / 1000
