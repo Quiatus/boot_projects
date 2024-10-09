cd58db2988804a184222f2f931306162b691bb86
quiatus <quiatus@gmail.com>
2024-10-09 22:25:52 +0200

    rotate player

--- 02-asteroids/main.py
+++ 02-asteroids/main.py
@@ -2,18 +2,22 @@ import pygame
 from constants import *
 from player import *
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-fps = pygame.time.Clock()
-dt = 0
 
 def main():
+
+  pygame.init()
+
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+  fps = pygame.time.Clock()
+  dt = 0
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+    
+    player.update(dt)
     screen.fill("black")
     player.draw(screen)
     pygame.display.flip()
